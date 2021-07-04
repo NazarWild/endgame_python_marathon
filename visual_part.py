196811e05c83042fe40e6c44256da31160e43db4
Nazar <53997458+NazarWild@users.noreply.github.com>
2021-07-04 17:23:40 +0300

    push

--- visual_part.py
+++ visual_part.py
@@ -34,7 +34,7 @@ def recForTree(tree, bId, big_data, type):
         if isinstance(key, list):
             string = f'{data}' + ": [" + f'{len(key)}' + "]"
             # print(f'list = {key}, {string}')
-            newId = tree.insert(bId, i, text=string)
+            newId = tree.insert(bId, i, text=string, background="#5F9EA0")
             recForTree(tree, newId, key, 1)
         elif isinstance(key, dict):
             if type == 1:
@@ -110,8 +110,8 @@ def render_packed(root, db=None):
 
 
     # create frames
-    frame1 = tk.Frame(notebook, bg="#F0E68C")
-    frame2 = tk.Frame(notebook, bg="#F0E68C")
+    frame1 = ttk.Frame(notebook)
+    frame2 = ttk.Frame(notebook)
 
     frame1.grid()
     frame2.grid()
@@ -122,20 +122,27 @@ def render_packed(root, db=None):
 
     tree = ttk.Treeview(frame1, height=100)
     tree.column('#0', width=820, stretch=YES)
-    tree.grid(row=0, rowspan=100, column=9, columnspan=8)
+    tree.grid(row=3, rowspan=100, column=9, columnspan=8)
+
+    #response view
+    r_view = tk.StringVar()
+    res_view = ttk.Combobox(frame1, textvariable=r_view, values=('TreeView', 'Table', 'Yaml', 'Raw', 'Json'))
+    res_view.current(0)
+    # res_view.bind('<<ComboboxSelected>>', views_changed)
+    res_view.grid(row=0, rowspan=3, column=9, columnspan=8)
 
     meth = tk.StringVar()
     methods = ttk.Combobox(frame1, textvariable=meth, values=('GET', 'POST', 'PATCH', 'PUT', 'DELETE'))
-    methods.current(1)
+    methods.current(0)
     # methods.bind('<<ComboboxSelected>>', methods_changed)
     methods.grid(row=0, column=0, columnspan=2)
 
     url = StringVar()
-    url_entry = tk.Entry(frame1, text='key', textvariable=url)
+    url_entry = ttk.Entry(frame1, text='key', textvariable=url)
     url_entry.grid(row=0, column=3, columnspan=4)
 
     #info label
-    info = tk.Label(frame1, text="", bg="#F0E68C", width=52)
+    info = tk.Label(frame1, text="", width=52, bg="#4682B4")
     info.grid(row=1, rowspan=3, column=0, columnspan=8)
 
     def send_request():
@@ -151,82 +158,74 @@ def render_packed(root, db=None):
             # if res_view.get() == "Table":
             #     funcForTable(tree, data)
 
-    send_button = tk.Button(frame1, text="SEND", command=send_request, bg="Gold")
+    send_button = ttk.Button(frame1, text="SEND", command=send_request)
     send_button.grid(row=0, column=7, columnspan=2)
 
     #parameters
-    f_params = tk.Frame(frame1, bg="#BDB76B")
+    f_params = ttk.Frame(frame1)
     f_params.grid(row=4, column=0, columnspan=8)
 
-    tk.Label(f_params, text="Params").grid(row=0, column=0)
+    ttk.Label(f_params, text="Params").grid(row=0, column=0)
 
     def add_params():
-        entryKey = tk.Entry(f_params)
+        entryKey = ttk.Entry(f_params)
         entryKey.grid(row=len(params) + 1, column=0)
-        entryValue = tk.Entry(f_params)
+        entryValue = ttk.Entry(f_params)
         entryValue.grid(row=len(params) + 1, column=4)
         params[entryKey] = entryValue
 
     params = {}
-    entryKey = tk.Entry(f_params)
+    entryKey = ttk.Entry(f_params)
     entryKey.grid(row=1, column=0)
-    entryValue = tk.Entry(f_params)
+    entryValue = ttk.Entry(f_params)
     entryValue.grid(row=1, column=4)
     params[entryKey] = entryValue
 
-    tk.Button(f_params, text="+", command=add_params).grid(row=1, column=8)
+    ttk.Button(f_params, text="+", command=add_params).grid(row=1, column=8)
 
     #body
-    f_body = tk.Frame(frame1, bg="#BDB76B")
+    f_body = ttk.Frame(frame1)
     f_body.grid(row=10, column=0, columnspan=8)
 
-    tk.Label(f_body, text="Body").grid(row=0, column=0)
+    ttk.Label(f_body, text="Body").grid(row=0, column=0)
 
     def add_body():
-        entryKeybody = tk.Entry(f_body)
+        entryKeybody = ttk.Entry(f_body)
         entryKeybody.grid(row=len(body) + 1, column=0)
-        entryValuebody = tk.Entry(f_body)
+        entryValuebody = ttk.Entry(f_body)
         entryValuebody.grid(row=len(body) + 1, column=4)
         body[entryKeybody] = entryValuebody
 
     body = {}
-    entryKeybody = tk.Entry(f_body)
+    entryKeybody = ttk.Entry(f_body)
     entryKeybody.grid(row=1, column=0)
-    entryValuebody = tk.Entry(f_body)
+    entryValuebody = ttk.Entry(f_body)
     entryValuebody.grid(row=1, column=4)
     body[entryKeybody] = entryValuebody
 
-    tk.Button(f_body, text="+", command=add_body).grid(row=1, column=8)
+    ttk.Button(f_body, text="+", command=add_body).grid(row=1, column=8)
 
     #headers
-    f_headers = tk.Frame(frame1, bg="#BDB76B")
+    f_headers = ttk.Frame(frame1)
     f_headers.grid(row=16, column=0, columnspan=8)
 
-    tk.Label(f_headers, text="Headers").grid(row=0, column=0)
+    ttk.Label(f_headers, text="Headers").grid(row=0, column=0)
 
     def add_headers():
-        entryKeyHeaders = tk.Entry(f_headers)
+        entryKeyHeaders = ttk.Entry(f_headers)
         entryKeyHeaders.grid(row=len(headers) + 1, column=0)
-        entryValueHeaders = tk.Entry(f_headers)
+        entryValueHeaders = ttk.Entry(f_headers)
         entryValueHeaders.grid(row=len(headers) + 1, column=4)
         headers[entryKeyHeaders] = entryValueHeaders
 
     headers = {}
-    entryKeyHeaders = tk.Entry(f_headers)
+    entryKeyHeaders = ttk.Entry(f_headers)
     entryKeyHeaders.grid(row=1, column=0)
-    entryValueHeaders = tk.Entry(f_headers)
+    entryValueHeaders = ttk.Entry(f_headers)
     entryValueHeaders.grid(row=1, column=4)
     headers[entryKeyHeaders] = entryValueHeaders
 
-    tk.Button(f_headers, text="+", command=add_headers).grid(row=1, column=8)
-
-
-    #response view
-    r_view = tk.StringVar()
-    res_view = ttk.Combobox(frame1, textvariable=r_view, values=('TreeView', 'Table', 'Yaml', 'Raw', 'Json'))
-    res_view.current(0)
-    # res_view.bind('<<ComboboxSelected>>', views_changed)
-    res_view.grid(row=22, rowspan=3, column=0, columnspan=8)
+    ttk.Button(f_headers, text="+", command=add_headers).grid(row=1, column=8)
 
     #history tab
     history = ttk.Treeview(frame2, selectmode='browse')
@@ -243,16 +242,22 @@ def render_packed(root, db=None):
 def visual_start(db):
     root = tk.Tk()
     root.title('Endgame')
-    root.geometry('1290x1200')
+    root.geometry('1300x1200')
     root.minsize(800, 800)
     root.eval('tk::PlaceWindow . center')
     root.configure(background='grey')
     root.resizable(False, False)
 
-    s = ttk.Style()
-    s.theme_use('default')
-    # ('aqua', 'step', 'clam', 'alt', 'default', 'classic')
-
     render_packed(root, db=db)
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure('TLabel', background='#87CEEB')
+    style.configure('TButton', background='#7B68EE', foreground='Black')
+    style.configure('TEntry', background='#AFEEEE')
+    style.configure('TFrame', background='#4682B4')
+    style.configure('Treeview', background='#FF00FF')
+    style.configure('TCombobox')
+    # ('aqua', 'step', 'clam', 'alt', 'default', 'classic')
 
     root.mainloop()
